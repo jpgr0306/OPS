365b6cd614941098ed3dab204b79fb701b702bbb
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2026-03-15 02:36:13 -0300

    Update dadosabertos.py

--- dadosabertos.py
+++ dadosabertos.py
@@ -1,16 +1,7 @@
 import requests
 
 urls = {
-    "PATENTES_CONTEUDO.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_CONTEUDO.csv",
-    "PATENTES_PRIORIDADES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_PRIORIDADES.csv",
-    "PATENTES_DEPOSITANTES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_DEPOSITANTES.csv",
-    "PATENTES_PROCURADORES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_PROCURADORES.csv",
-    "PATENTES_CLASSIFICACAO_IPC.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_CLASSIFICACAO_IPC.csv",
-    "PATENTES_INVENTORES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_INVENTORES.csv",
-    "PATENTES_DESPACHOS.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_DESPACHOS.csv",
-    "PATENTES_VINCULOS.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_VINCULOS.csv",
-    "PATENTES_RENUMERACOES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_RENUMERACOES.csv",
-    "PATENTES_DADOS_BIBLIOGRAFICOS.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_DADOS_BIBLIOGRAFICOS.csv",
+    "PATENTES_PRIORIDADES.csv": "https://dadosabertos.inpi.gov.br/download/patentes/PATENTES_PRIORIDADES.csv"
 }
 
 for nome, url in urls.items():
